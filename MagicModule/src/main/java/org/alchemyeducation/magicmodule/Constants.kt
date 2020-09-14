2d0c1a74d069f3afbb7465449afa43171bf51f2b
Mayank Sharma <mayank.sharma@nowfloats.com>
2020-09-14 20:18:41 +0530

    Initial commit

---
+++ MagicModule/src/main/java/org/alchemyeducation/magicmodule/Constants.kt
@@ -0,0 +1,4 @@
+package org.alchemyeducation.magicmodule
+
+object Constants {
+}
---
+++ MagicModule/src/main/java/org/alchemyeducation/magicmodule/helpers/NetworkHelper.kt
@@ -0,0 +1,16 @@
+package org.alchemyeducation.magicmodule.helpers
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+
+
+object NetworkHelper {
+
+    fun isNetworkConnected(context: Context): Boolean {
+        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
+        return activeNetwork?.isConnectedOrConnecting == true
+    }
+
+}
